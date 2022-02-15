86ac231e1d279eee3a263b0040abf8001fbf9705
Jan Hellar <janhellar@gmail.com>
2022-02-15 20:22:08 +0100

    update monaco-editor

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 const config = {
   paths: {
-    vs: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.30.1/min/vs',
+    vs: "https://cdn.jsdelivr.net/npm/monaco-editor@0.32.1/min/vs",
   },
-}
+};
 
 export default config;
